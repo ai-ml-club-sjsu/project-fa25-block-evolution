94d029cf8385cd7d82c49eb58941baa29fd5a036
Aili Nierengarten <125620224+8000thCube@users.noreply.github.com>
2025-10-23 20:10:02 -0700

    Update evolution.rs

--- src/evolution.rs
+++ src/evolution.rs
@@ -37,32 +37,55 @@ impl Population{
 		loss.resize(size,f32::INFINITY);
 
 		let mut filename=String::with_capacity(directory.len()+10);
-		let results:Vec<Option<(Graph<Layer<NdArray>>,Vec<Vec<u32>>,f32)>>=(0..size).into_iter().map(|n|{
+		let mut results:Vec<Option<(Graph<Layer<NdArray>>,Vec<u32>,f32)>>=(0..size).into_iter().map(|n|{
 			filename.clear();
 
 			let _ok=write!(&mut filename,"{directory}/model{n}").inspect_err(|e|println!("failed to generate file name: {e}")).ok()?;
 
 			let _ok=write(&mut BufWriter::new(File::create(&filename).inspect_err(|e|println!("failed to create gene file: {e}")).ok()?),&genes[n]);
 
-			let status=Command::new(program).arg(&filename).output().map(|status|status.status.success()).inspect_err(|e|println!("failed to evaluate model: {e}")).ok()?;
+			let mut program=Command::new(program).arg(&filename).spawn().inspect_err(|e|println!("failed to start evaluation program: {e}")).ok()?;
+			let start=Instant::now();
+
+			loop{
+				if let Ok(Some(status))=program.try_wait(){
+					if !status.success(){println!("evaluation program didn't exit successfully")}
+					break;
+				}else{
+					if start.elapsed()>*timeout{
+						println!("evaluation program timed out");
+						program.kill().ok();
+					}
+					thread::sleep(Duration::from_millis(50));
+				}
+			}
+
 			from_read(BufReader::new(File::open(&filename).inspect_err(|e|println!("failed to open result file: {e}")).ok()?)).inspect_err(|e|println!("failed to read result file: {e}")).ok()
 		}).collect();
 
-		todo!()
+		results.extend(models.drain(..).zip(genes.drain(..)).zip(loss.drain(..)).map(|((model,gene),loss)|Some((model,gene,loss))));
+		results.sort_unstable_by(|a,b|{
+			let (a,b)=(if let Some((_graph,_gene,loss))=a{*loss}else{f32::INFINITY},if let Some((_graph,_gene,loss))=b{*loss}else{f32::INFINITY});
+			let (a,b)=(if a.is_nan(){f32::INFINITY}else{a},if b.is_nan(){f32::INFINITY}else{b});
+
+			a.total_cmp(&b)
+		});
+
+		for (m,g,l) in results.into_iter().map_while(|x|x).take(capacity){
+			genes.push(g);
+			loss.push(l);
+			models.push(m);
+		}
 	}
 }
 #[derive(Clone,Debug,Deserialize,Serialize)]
 /// structure for storing a population of models and calling a subprocess to evaluate each one. The subprocess program should accept an argument for a directory to save the model and the evaluation result
 pub struct Population{capacity:usize,directory:String,genes:Vec<Vec<u32>>,loss:Vec<f32>,models:Vec<Graph<Layer<NdArray>>>,program:String,threads:usize,timeout:Duration}
-use block_graph::{
-	AI,Graph,Op,Unvec,UnwrapInner,burn::{Layer,LossOutput,Shortcuts,TrainConfig,Value,Wrappable}
-};
-use burn::{
-	backend::{Autodiff,NdArray},data::dataset::{Dataset,vision::MnistDataset},module::AutodiffModule,optim::AdamWConfig,prelude::Backend
-};
+use block_graph::{Graph,burn::Layer};
+use burn::backend::NdArray;
 use crate::gene;
 use rmp_serde::{decode::from_read,encode::write};
 use serde::{Deserialize,Serialize};
 use std::{
-	fmt::Write as FmtWrite,fs::{File,create_dir_all},mem,io::{BufReader,BufWriter,Read,Write as IOWrite},process::Command,time::Duration
+	fmt::Write as FmtWrite,fs::{File,create_dir_all},io::{BufReader,BufWriter},process::Command,thread,time::{Duration,Instant}
 };
